ccae7164ce07ef9626dbb7d8829f4ac54d6f0f51
vic <vichsu38@gmail.com>
2021-11-08 19:22:19 +0800

    spring

---
+++ SpringBoot-Portfolio-20211018/src/main/java/com/example/demo/SpringBootPortfolio20211018Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPortfolio20211018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPortfolio20211018Application.class, args);
+	}
+
+}
---
+++ SpringBoot-Portfolio-20211018/src/test/java/com/example/demo/SpringBootPortfolio20211018ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPortfolio20211018ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
